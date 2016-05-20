da822369a23d8ee3ed0436a407c94c56dd056b48
Joe M <jmarshall@codero.com>
2016-05-20 18:16:07 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const config = require('./config.js');
+const Twit = require('twit');
+const fs = require('fs');
+const Bot = new Twit(config);
+var schedule = require('node-schedule');
+
+var rule = new schedule.RecurrenceRule();
+rule.minute = 14;
+
+var j = schedule.scheduleJob(rule, composePost);
+
+function tweet (content) {
+	Bot.post('statuses/update', { status: content }, function(err, reply){
+		if (err) {
+			console.log(err);
+			return;	
+		}
+	})
+};
+
+function composeTweet(content) {
+	return content + " #grrmbot"; 
+}
+
+function openText () {
+	return fs.readFileSync('soiaf.txt', 'utf8');
+}
+
+function writeText(string) {
+	fs.writeFileSync('soiaf.txt', string, 'utf8');
+}
+
+function removeString(string, text) {
+	return text.replace(string, "");
+}
+
+function pullContent() {
+	let text = openText();
+	let sentences = text.split('.');
+	let sentence = sentences.shift();
+	let finalText = sentences.join('.');
+	writeText(finalText);
+	return sentence;
+}
+
+function composePost() {
+	let phrase = pullContent()
+	let clean_phrase = phrase.replace('\n', '').trim();
+	if (clean_phrase.length < 130) {
+		tweet(composeTweet(clean_phrase));
+	} else {
+		composePost()
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
